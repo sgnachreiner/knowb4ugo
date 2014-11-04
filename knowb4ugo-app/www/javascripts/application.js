2b62974a18f822bbb5b44d10d4c9019e10f07d1c
BrianLichliter <brian.lichliter@gmail.com>
2014-11-04 10:43:40 -0600

    removed node_modules from .gitignore

--- knowb4ugo-app/www/javascripts/application.js
+++ knowb4ugo-app/www/javascripts/application.js
@@ -1,5 +1,5 @@
 // Display the native navigation bar with the title "Hello World!"
-steroids.view.navigationBar.show("Hello World!");
+steroids.view.navigationBar.show("Know Before You Go");
 
 // Set the WebView background color to white (effective on iOS only)
 steroids.view.setBackgroundColor("#FFFFFF");
