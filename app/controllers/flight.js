e187d447f4323557236888b114aa6a73e383a723
Chen Lu <Chengaroo@dhcp-10-101-63-56.wireless.northwestern.private>
2014-11-28 20:47:14 -0600

    gets all data of flights added

--- app/controllers/flight.js
+++ app/controllers/flight.js
@@ -44,23 +44,45 @@ app.controller('FetchCtrl', function($scope, $http, $templateCache, $filter, $ro
     $scope.flights = JSON.parse(localStorage.getItem("flights"));
     $scope.method = 'JSONP';
     
-    $scope.fetch = function(num) {
-      $scope.flightNumber = $scope.flights[num].flightNum;
-      $scope.date = $filter('date')($scope.flights[num].departureDate, "yyyy/MM/dd");
+    $scope.fetch = function() {
+      //
+      $scope.flightNumber = $scope.flights[0].flightNum;
+      $scope.date = $filter('date')($scope.flights[0].departureDate, "yyyy/MM/dd");
       $scope.url = 'https://api.flightstats.com/flex/flightstatus/rest/v2/jsonp/flight/tracks/AA/'+ $scope.flightNumber +'/dep/'+ $scope.date +'?appId=c7c9c4f0&appKey=cacf8348266684a0eaeaef6dc3722402&utc=false&includeFlightPlan=false&maxPositions=2&callback=JSON_CALLBACK';
-      steroids.logger.log($scope.url);
+
       $scope.code = null;
       $scope.response = null;
 
       $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
         success(function(data, status) {
           $scope.status = status;
-          $scope.data = data;
+          $scope.data = [];
+          $scope.data.push(data);
         }).
         error(function(data, status) {
           $scope.data = data || "Request failed";
           $scope.status = status;
       });
+
+      if($scope.flights.length > 1 ){
+        for(var i = 1; i < $scope.flights.length; i++) {
+          $scope.flightNumber = $scope.flights[i].flightNum;
+          $scope.date = $filter('date')($scope.flights[i].departureDate, "yyyy/MM/dd");
+          $scope.url = 'https://api.flightstats.com/flex/flightstatus/rest/v2/jsonp/flight/tracks/AA/'+ $scope.flightNumber +'/dep/'+ $scope.date +'?appId=c7c9c4f0&appKey=cacf8348266684a0eaeaef6dc3722402&utc=false&includeFlightPlan=false&maxPositions=2&callback=JSON_CALLBACK';
+
+
+          $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
+            success(function(data, status) {
+              $scope.status = status;
+              $scope.data.push(data);
+            }).
+            error(function(data, status) {
+              $scope.data = data || "Request failed";
+              $scope.status = status;
+          });
+        }
+      }
+
     };
 
     $scope.showForm = function() {
